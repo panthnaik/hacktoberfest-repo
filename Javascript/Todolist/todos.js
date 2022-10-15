45d8aba76a289f4149ed0b6d13b3d5f0cf5fa94f
CodingNinja678 <SuhaneeHingorani@gmail.com>
2022-10-15 22:29:32 +0530

    JS Todo List

---
+++ Javascript/Todolist/todos.js
@@ -0,0 +1,25 @@
+let input = prompt('What would you like to do?');
+const todos = ['Clean my room', 'Throw the trash out'];
+while (input !== 'quit' && input !== 'q') {
+    if (input === 'list') {
+        console.log('*****************')
+        for (let i = 0; i < todos.length; i++) {
+            console.log(`${i}: ${todos[i]}`);
+        }
+        console.log('*****************')
+    } else if (input === 'new') {
+        const newTodo = prompt('What is the new todo?');
+        todos.push(newTodo);
+        console.log(`${newTodo} added to the list!`)
+    } else if (input === 'delete') {
+        const index = parseInt(prompt('Please enter an index to delete:'));
+        if (!Number.isNaN(index)) {
+            const deleted = todos.splice(index, 1);
+            console.log(`Ok, deleted ${deleted[0]}`);
+        } else {
+            console.log('Index not found')
+        }
+    }
+    input = prompt('What would you like to do?')
+}
+console.log('OK QUIT THE APP!')
